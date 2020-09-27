77af4ccd90d32eecaeeb2da6a85607a55fb93a3d
tjcichra <tjcichra@yahoo.com>
2020-09-26 23:46:55 -0400

    Added box to signup page

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,8 +1,34 @@
 <template>
   <div id="app">
     <h1>You would sign in here</h1>
+    <b-container class="container">
+      <h3 style="text-align: center;">Sign in</h3>
+      <div id="my-signin2"></div>
+    </b-container>
   </div>
 </template>
+
+<script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
+
+<script>
+  function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
+  function onFailure(error) {
+    console.log(error);
+  }
+  function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  }
+</script>
 
 <script>
 export default {
@@ -20,5 +46,11 @@ export default {
   background-color: rgb(129, 115, 148);
   font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
     "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+}
+
+.container {
+  border: 2px solid;
+  border-radius: 10px;
+  background-color: rgb(140, 133, 150);
 }
 </style>
