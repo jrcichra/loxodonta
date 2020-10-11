b0d5bebe9b8a40b7476ce0168d75649a32b8464f
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-10-11 13:39:36 -0400

    loxodonta server prototype

--- plugins/vue-google-oauth2.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import GAuth from 'vue-google-oauth2'
-
-const gauthOption = {
-    clientId: 'CLIENT_ID.apps.googleusercontent.com',
-    scope: 'profile email',
-    prompt: 'select_account'
-}
-Vue.use(GAuth, gauthOption)
---
+++ server/server.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+// Some fake data
+
+const posts = [
+    {
+        id: 0,
+        created: 1506045,
+        user_id: 0,
+        text: "This is a post by Justin",
+        object_set_id: undefined,
+        edited: undefined,
+        views: 5,
+        upvotes: 2,
+        downvotes: 3,
+        parent: undefined
+    },
+    {
+        id: 1,
+        created: 1504567,
+        user_id: 1,
+        text: "This is a post by Tim with pictures",
+        object_set_id: 0,
+        edited: undefined,
+        views: 10,
+        upvotes: 200,
+        downvotes: 1,
+        parent: undefined
+    },
+    {
+        id: 2,
+        created: 1504569,
+        user_id: 0,
+        text: "This is a comment Justin put on Tim's popular post",
+        object_set_id: 0,
+        edited: undefined,
+        views: 2,
+        upvotes: 2,
+        downvotes: 100,
+        parent: 1
+    }
+];
+
+const users = [
+    {
+        id: 0,
+        created: 34534,
+        username: 'jrcichra',
+        bio: 'Pretty cool guy',
+        status: 'online',
+        avatar: 'https://loremflickr.com/320/240',
+        // posts: posts.filter(post => post.user_id === 0)
+    },
+    {
+        id: 1,
+        created: 34243,
+        username: 'tjcichra',
+        bio: 'A brother',
+        status: 'offline',
+        avatar: 'https://loremflickr.com/320/240',
+        // posts: posts.filter(post => post.user_id === 1)
+    },
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { user(id: ID!): User }
+  type User  { id: ID!, created: Int, username: String, bio: String, status: String, avatar: String, posts: [Post] }
+  type Post { id: ID!, created: Int, user_id: Int, text: String, object_set_id: Int, edited: Int, views: Int, upvotes: Int, downvotes: Int, Parent: Int }
+`;
+
+// The resolvers
+const resolvers = {
+    Query: {
+        user: (parent, args, context, info) => {
+            return users.find(user => user.id === Number(args.id));
+        },
+        // posts: () => posts,
+    },
+    User: {
+        posts: (parent, args, context, info) => {
+            console.log(posts.filter(post => post.user_id === Number(parent.id)))
+            return posts.filter(post => post.user_id === Number(parent.id))
+        }
+    }
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
+
+// Initialize the app
+const app = express();
+
+// The GraphQL endpoint
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+// GraphiQL, a visual editor for queries
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Go to http://localhost:3000/graphiql to run queries!');
+});
